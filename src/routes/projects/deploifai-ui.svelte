434d4aa796defea090e87805900746051c3042eb
Seyoung Park <park_seyoung@outlook.com>
2021-09-27 17:24:19 +0800

    change p font and about page layout

--- src/routes/projects/deploifai-ui.svelte
+++ src/routes/projects/deploifai-ui.svelte
@@ -5,7 +5,7 @@
 
 <div class="container">
 <h2 class="heading"> Lorem ipsum dolor sit amet</h2>
-<h2 class="Subheading"> consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </h2>
+<p class="Subheading"> consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
 <img id="image1" src="/thumbnails/deploifai.png" alt="deploifai landing page"/>
 <p class="description">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
